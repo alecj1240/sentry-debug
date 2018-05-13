52cf048ae6b49040dceb8525f8e1f981d9a016b2
Alec Jones <alecj1240@hotmail.com>
2018-05-12 22:31:02 -0700

    added devise to app

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'httparty'
 gem 'faraday'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    #conn = Faraday.new(:url => "https://app.getsentry.com")
+    #@stackResults = conn.get '/api/0/projects/christopher-bot/christopher-bot/events/', { :Authorization => "2ff6957e5241427fa6a6cd4d3d72c964196635870b2c4eac9435ad1e0e18ba19", :Host => "app.getsentry.com" }
+    #return @stackResults
   end
 end
