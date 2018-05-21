eb4191882a8b57268094d8bf24d7c9fe3dcf32d6
Alec Jones <alecj1240@hotmail.com>
2018-05-20 18:26:55 -0700

    added token page and token to db for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
     #@stackResults = conn.get '/api/0/projects/christopher-bot/christopher-bot/events/', { :Authorization => "2ff6957e5241427fa6a6cd4d3d72c964196635870b2c4eac9435ad1e0e18ba19", :Host => "app.getsentry.com" }
     #return @stackResults
   end
+
+  def token
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def token
-  end
-
   def after_sign_up_path_for(resource)
-    redirect_to token_path # Or :prefix_to_your_route
+    token_path # Or :prefix_to_your_route
   end
 end
