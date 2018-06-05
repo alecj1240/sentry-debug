6590ff74f5da484d2456d043a4bfa87e1c10f772
Alec Jones <alecj1240@hotmail.com>
2018-06-04 21:39:36 -0700

    created show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,6 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to token_path if current_user.sentry_token.nil?
       @sentryErrors = current_user.sentryerrors
-=begin
-      uri = URI.parse("https://www.googleapis.com/customsearch/v1?q=NoMethodError+update&cx=001638859173749984711:glesaa6k8uo&key=AIzaSyBQBjc5W-ACdzZW3zK_eq48s4NbwGrEWLQ")
-      request = Net::HTTP::Get.new(uri)
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-      end
-      @googleResults = JSON.parse(response.body)
-      puts @googleResults
-=end
-
     end
   end
 
@@ -30,5 +17,16 @@ class PagesController < ApplicationController
       current_user.update(:sentry_token => params[:user][:sentry_token])
       redirect_to root_path
     end
+  end
+
+  def show
+    if user_signed_in?
+      @sentryerror = current_user.sentryerrors.find_by(id: params[:error_id])
+    else
+      redirect_to unauthorized_path
+    end
+  end
+
+  def unauthorized
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root 'pages#index'
   get 'token' => 'pages#token'
+  get 'show' => 'pages#show'
+  get 'unauthorized' => 'pages#unauthorized'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
