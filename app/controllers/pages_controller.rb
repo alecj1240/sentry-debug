92e76628be3968de40e3ac7f4eda4fc122dddf0a
Alec Jones <alecj1240@hotmail.com>
2018-06-05 22:05:08 -0700

    making additions with modals and stack api - going well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,4 +29,27 @@ class PagesController < ApplicationController
 
   def unauthorized
   end
+
+  # function to display the best answer to the user
+  def get_answer(link)
+    @question_id = link.split('/')[4]
+    puts "THE QUESTION ID"
+    puts link
+    puts @question_id
+    puts "THE QUESTION ID"
+    uri = URI.parse("https://api.stackexchange.com/2.2/questions/#{@question_id}/answers?order=desc&sort=activity&site=stackoverflow")
+    request = Net::HTTP::Get.new(uri)
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    puts JSON.parse(response.body)
+    puts "------------------------"
+    puts "------------------------"
+    puts "------------------------"
+  end
+  helper_method :get_answer
 end
